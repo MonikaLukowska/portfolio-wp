cce05a17f6bf9fbf0a3988bf0e7f1082b471b763
Monia <monia@pcs-macbook-pro.home>
2021-07-22 22:26:23 +0200

    html main content total

---
+++ front-page.php
@@ -0,0 +1,193 @@
+<?php
+/**
+ * 
+ */
+
+get_header();
+?>
+<main class="main-content">
+  <section class="hero">
+    <div class="container container--wide hero__container">
+      <div class="hero__row">
+        <div class="hero__col hero__content">
+          <h2 class="hero__content__heading heading site-heading"><span class="bold">Strona www</span>dla Twojego biznesu<span class="pink">.</span></h2>
+          <ul class="hero__content__features">
+            <li class="hero__content__features__item hero__content__features__item--first">Funkcjonalna</li>
+            <li class="hero__content__features__item hero__content__features__item--second">Responsywna</li>
+            <li class="hero__content__features__item hero__content__features__item--third">Optymalna</li>
+            <li class="hero__content__features__item hero__content__features__item--fourth">Nowoczesna</li>
+            <li class="hero__content__features__item hero__content__features__item--fifth">Atrakcyjna</li>
+          </ul>
+          <button class="btn" aria-label="Go to form">Wycena
+            <span class="arrow">
+            <svg xmlns="http://www.w3.org/2000/svg" width="36.433" height="17.064" viewBox="0 0 36.433 17.064">
+  <g id="Group_35" data-name="Group 35" transform="translate(-783 -3407.227)">
+    <g id="Path_7" data-name="Path 7" transform="translate(783 3414)" fill="#020ca3">
+      <path d="M 33.22978591918945 2.5 L 26.57699775695801 2.5 L 0.4999968707561493 2.5 L 0.4999968707561493 0.5 L 33.16619873046875 0.5 L 34.28131866455078 1.748761534690857 L 33.22978591918945 2.5 Z" stroke="none"/>
+      <path d="M 0.9999961853027344 1 L 0.9999961853027344 2 L 26.57699584960938 2 L 33.06953430175781 2 L 33.53706359863281 1.665986895561218 L 32.94234848022461 1 L 0.9999961853027344 1 M -3.814697265625e-06 0 L 33.39004516601562 0 L 35.02557754516602 1.831539988517761 L 33.39004516601562 3 L 26.57699584960938 3 L -3.814697265625e-06 3 L -3.814697265625e-06 0 Z" stroke="none" fill="#020ca3"/>
+    </g>
+    <g id="Path_9" data-name="Path 9" transform="matrix(0.883, -0.469, 0.469, 0.883, 804.575, 3420.878)" fill="#020ca3">
+      <path d="M 1.101491570472717 3.355393648147583 L 0.7051412463188171 1.13896918296814 L 13.56271457672119 0.5150710344314575 L 13.98150730133057 1.335362911224365 L 1.101491570472717 3.355393648147583 Z" stroke="none"/>
+      <path d="M 12.44811153411865 1.06974196434021 L 1.297453880310059 1.610814094543457 L 1.507530212402344 2.785601377487183 L 12.44811153411865 1.06974196434021 M 13.86114406585693 4.291534423828125e-06 L 14.74051380157471 1.722434282302856 L 0.6954441070556641 3.925184488296509 L 0.1128339767456055 0.6671242713928223 L 13.86114406585693 4.291534423828125e-06 Z" stroke="none" fill="#020ca3"/>
+    </g>
+    <g id="Path_8" data-name="Path 8" transform="translate(807.096 3407.227) rotate(32)" fill="#020ca3">
+      <path d="M 12.61969757080078 2.5 L 0.5000043511390686 2.5 L 0.5000043511390686 0.5222271084785461 L 13.01680755615234 1.066752076148987 L 12.61969757080078 2.5 Z" stroke="none"/>
+      <path d="M 1.000004768371582 1.044454216957092 L 1.000004768371582 2 L 12.23940372467041 2 L 12.36714267730713 1.538964748382568 L 1.000004768371582 1.044454216957092 M 4.76837158203125e-06 0 L 13.66648387908936 0.5945401191711426 L 13.00000476837158 3 L 4.76837158203125e-06 3 L 4.76837158203125e-06 0 Z" stroke="none" fill="#020ca3"/>
+    </g>
+  </g>
+</svg>
+            </span>
+          </button>
+        </div>
+        <div class="hero__decorators">
+          <div class="hero__decorators__blob"><img src="/wp-content/themes/frona/images/blob.png" alt="" class="hero__decorators__blob"></div>
+          <div class="hero__decorators__mac">
+            <img src="/wp-content/themes/frona/images/mac.png" alt="" class="hero__decorators__mac__img">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div class="container">
+    <section id="services" class="offer">
+      <h2 class="site-heading site-heading--large center">Oferta</h2>
+      <ul class="offer__blocks">
+        <li class="offer__block">
+          <div class="offer__block__icon-container">
+            <img src="/wp-content/themes/frona/images/design.svg" alt=""  loading="lazy" class="offer__block__icon offer__block__icon--design">
+          </div>
+          <h3 class="site-heading site-heading--medium">Web design</h3>
+          <p class="offer__block__content generic">Tworzę nowoczesne i atrakcyjne wizualnie strony i sklepy www. Dzięki współpracy z doświadczonymi grafikami i UX/UI designerami dbam o wygląd i funkcjonalności dostosowane do wygody i intuicji uzytkowników.</p>
+        </li>
+        <li class="offer__block">
+          <div class="offer__block__icon-container">
+            <img src="/wp-content/themes/frona/images/wp.svg" alt="" loading="lazy" class="offer__block__icon offer__block__icon--wp">
+          </div>
+          <h3 class="site-heading site-heading--medium">CMS Wordpress</h3>
+          <p class="offer__block__content generic">Klientom oczekującym możliwości samodzielnej edycji treści proponuję strony bazujące na CMS Wordpress, z intuicyjnym panelem administracyjnym. W zaleności od potrzeb i budżetu klienta oferuję szablon customowy lub opieram się na gotowym motywie.</p>
+        </li>
+        <li class="offer__block">
+          <div class="offer__block__icon-container">
+            <img src="/wp-content/themes/frona/images/dev.svg" alt="" loading="lazy" class="offer__block__icon offer__block__icon--dev">
+          </div>
+          <h3 class="site-heading site-heading--medium">Web development</h3>
+          <p class="offer__block__content generic">Masz już projekt przygotowany przez grafika i szukasz programisty, który Ci go zakoduje? Znalazłeś! Zamienię Twój projekt na czysty i przejrzysty kod, z dbałością o dobre praktyki SEO i czybkość ładowania.</p>
+        </li>
+        <li class="offer__block">
+          <div class="offer__block__icon-container">
+            <img src="/wp-content/themes/frona/images/admin.svg" alt="" loading="lazy"  class="offer__block__icon offer__block__icon--admin">
+          </div>
+          <h3 class="site-heading site-heading--medium">Administracja</h3>
+          <p class="offer__block__content generic">Proponuję Ci stałą opiekę nad stroną, w tym wszelkie aktualizacje, backup i wprowadzanie zmian, a Ty zaoszczędzony czas możesz przeznaczyć na prowadzenie biznesu.</p>
+        </li>
+      </ul>
+    </section>
+    <section id="work" class="portfolio">
+      <h2 class="site-heading site-heading--large center">Portfolio</h2>
+    <?php
+      if(have_rows('portfolio')):?>
+      <div class="portfolio__grid">
+        <?php
+        while(have_rows('portfolio')): the_row();
+        $mock = get_sub_field('image');
+        $full = get_sub_field('full');
+    ?>
+        <div class="portfolio__grid__item">
+          <div class="portfolio__grid__img-wrapper">
+            <img class="portfolio__grid__img" src="<?php echo $mock['sizes']['medium_large']?>" alt="<?php echo $mock['alt']?>" loading="lazy"></img>
+          </div>
+          <div class="portfolio__grid__bottom">
+            <div class="portfolio__grid__desc"><?php echo get_sub_field('desc')?></div>
+            <button class="btn btn--small portfolio__grid__view" aria-label="Zobacz" data-src="<?php echo $mock['url']?>" data-link="<?php echo get_sub_field('link')?>">Zobacz
+              <span class="arrow">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="55.325" height="17.056" viewBox="0 0 55.325 17.056">
+                  <defs>
+                    <pattern id="pattern" width="1" height="1" patternTransform="matrix(1, 0, 0, -1, 0, 34.111)" viewBox="0 0 55.325 17.056">
+                      <image preserveAspectRatio="none" width="55.325" height="17.056" xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC4AAAAOCAYAAABQFS4BAAAAAXNSR0IArs4c6QAAADhlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAAqACAAQAAAABAAAALqADAAQAAAABAAAADgAAAACwmyk6AAABLklEQVRIDc2UsU4CQRCGOSKxpbGgMDExkYLGAjmvt6E0NjS8gi9BR6NvYEd8Dwu9yoSExmjh0RBosKXg/GazbAZCc5dsbjf5b2c3uzPf/ZlslOd5LdRx3nl9hu1pPhv8HjJGIYIDfAHoBCXoB3WBXzO7UXdRWEELHIGWcYlSfqZpVvYTJDjuvsN3p0CviPfgg2yVHTAunxG/obbd+2KOpW1KgZPw0SbyOcmrkaEhelCFBD4pDA50xMWtSlRFuCgMLpTAV/2Gbk60XeIm/eOgrLvmiN5nY6TveYzn5O6je1VjSXxbynGVxHuIeSlFeraQQN9gYrbnuHeKAgUAvub4Bzq11xy0rIN8x4GOYftER6GDBQfsD30LIGOFTHuYlf0E2+O43oDxBY3p6amGlvgf1vJjGq8v0woAAAAASUVORK5CYII="/>
+                    </pattern>
+                  </defs>
+                  <rect id="right-arrow_kopia_2" data-name="right-arrow kopia 2" width="55.325" height="17.056" fill="url(#pattern)"/>
+                </svg>
+              </span>
+            </button>
+          </div>
+        </div>
+    <?php endwhile;?>
+    </div>
+    <?php endif;?>
+    </section>
+    <section class="process" id="process">
+      <div class="process__time-line">
+        <div class="process__time-line__step process__time-line__step--1 process__time-line__right">
+          <div class="process__time-line__desc">
+            <h3 class="heading heading--small">Kontakt z klientem</h3>
+            <p class="generic">To etap na którym dopracowujemy szczegóły przyszłej współpracy</p>
+          </div>
+          <div class="proces__time-line__icon-wrapper">
+            <img src="/wp-content/themes/frona/images/proces--1.svg" alt="" class="process__time-line__icon">
+          </div>
+        </div>
+        <div class="process__time-line__line"></div>
+        <div class="process__time-line__step process__time-line__step--2 process__time-line__left">
+          <div class="process__time-line__desc">
+            <h3 class="heading heading--small">Research</h3>
+            <p class="generic">Na tym etapie poznajemy się lepiej, badamy oczekiwania i potrzeby.</p>
+          </div>
+          <div class="proces__time-line__icon-wrapper">
+            <img src="/wp-content/themes/frona/images/proces-2.svg" alt="" class="process__time-line__icon">
+          </div>
+        </div>
+        <div class="process__time-line__line"></div>
+        <div class="process__time-line__step process__time-line__step--1 process__time-line__right">
+          <div class="process__time-line__desc">
+            <h3 class="heading heading--small">Projekt(opcjonalnie)</h3>
+            <p class="generic">Jest to etap, w którym powstaje ewentualny projekt graficzny</p>
+          </div>
+          <div class="proces__time-line__icon-wrapper">
+            <img src="/wp-content/themes/frona/images/proces-3.svg" alt="" class="process__time-line__icon">
+          </div>
+        </div>
+        <div class="process__time-line__line"></div>
+        <div class="process__time-line__step process__time-line__step--1 process__time-line__left">
+          <div class="process__time-line__desc">
+            <h3 class="heading heading--small">Wdrożenie</h3>
+            <p class="generic">Na tym etapie następuje wdrożenie projektu</p>
+          </div>
+          <div class="proces__time-line__icon-wrapper">
+            <img src="/wp-content/themes/frona/images/proces-4.svg" alt="" class="process__time-line__icon">
+          </div>
+        </div>
+        <div class="process__time-line__line"></div>
+        <div class="process__time-line__step process__time-line__step--1 process__time-line__right">
+          <div class="process__time-line__desc">
+            <h3 class="heading heading--small">Testy i implementacja</h3>
+            <p class="generic">To etap, w którym przeprowadzane są testy i wprowadzane ewentualne korekty przed przekazaniem i umieszczeniem na serwerze</p>
+          </div>
+          <div class="proces__time-line__icon-wrapper">
+            <img src="/wp-content/themes/frona/images/proces-5.svg" alt="" class="process__time-line__icon">
+          </div>
+        </div>
+        <div class="process__time-line__line"></div>
+      </div>
+    </section>
+    <section id="about" class="about">
+      <h2 class="heading heading--large">O mnie</h2>
+      <div class="about__images">
+        <pictrue class="about__picture">
+          <source media="(min-width:500px)" srcset="/wp-content/themes/frona/images/about-group.png" loading="lazy" alt="Programistka Fronaweb">
+          <img src="/wp-content/themes/frona/images/about-single.png" loading="lazy" alt="Programistka Fronaweb">
+        </pictrue>
+      </div>
+      <div class="about-desc">
+        <h3 class="about__heading">Cześć, mam na imię Monika i jestem web developerem freelancerem z Bydgoszczy.</h3>
+        <p class="about__generic generic">Tworzę strony dopasowane do oczekiwań, potrzeb i budżetów moich klientów. Początek współpracy jest więc dla mnie czasem na zbudowanie relacji opartej na efektywnej komunikacji i wzajemnym zaufaniu.</p>
+        <p class="about__generic generic">Jestem perfekcjonistką i wierzę, że nigdy nie jest się wystarczająco dobrym, dlatego nie przestaję się uczyć i rozwijać. Swoje projekty realizuję z pasją i pełnym zaangażowaniem.</p>
+        <p class="about__generic generic">Jeśli szukasz kogoś, kto wykona dla Ciebie stronę w uzgodnionym terminie, w uczciwej cenie, biorąc pod uwagę Twoje wyobrażenia i przy okazji włoży w to serce, to właśnie znalazłeś. Napisz do mnie lub zadzwoń - wspólnie stworzymy Twój pozytywny wizerunek w sieci.</p>
+      </div>
+    </section>
+  </div>
+</main> 
+<?php
+get_footer();
+
+?>
