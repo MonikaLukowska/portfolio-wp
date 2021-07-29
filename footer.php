995c6be2f3973668ccea8fb21070496a63eed732
Monia <monia@pcs-macbook-pro.home>
2021-07-29 20:10:45 +0200

    footer html

--- footer.php
+++ footer.php
@@ -1,4 +1,37 @@
-
+<footer class="footer">
+  <div class="container container--wide">
+    <div class="footer__row">
+      <div class="footer__form form">
+        <?php echo do_shortcode('[contact-form-7 id="40" title="Contact form 1"]')?>
+      </div>
+      <div class="footer__contact">
+        <div class="footer__contact__logo-wrapper">
+          <img class="footer__contact__logo" src="/wp-content/themes/frona/images/logo.svg">
+        </div>
+        <div class="footer__contact__details">
+          <p class="footer__contact__details__tel">
+            <a class="footer__contact__link" href="tel:0048734164767">
+              <span role="img" aria-label="telephone" class="footer__icon icon icon--tel">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="14" height="26" viewBox="0 0 14 26">
+                  <image id="smartphone" width="14" height="26" xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA4AAAAaCAYAAACHD21cAAAAAXNSR0IArs4c6QAAADhlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAAqACAAQAAAABAAAADqADAAQAAAABAAAAGgAAAAALWCqCAAABFUlEQVQ4Ee2UP2oCQRjFd6MIWRUiJGKbC3iCNBbpxAuYPpAD5BiWQQurFN4itVhY5QJaCkLAP0FcY35vnY9dNmI2XQof/Ob7ZmbfOIzw/OCm2/I8rwNVkHIQRF087GmXUII1jH2MG5oCJNVPTGSqQRNe4Am8PFyoSenezX2qjJdubus7GbX5CjpN0lXLUXcYtB/CAq7hA55l1Ifvq9njkJpJxWqvbdesZHLEHxXNGC9l7M7GEw91fpx/+ThKgb8otP8xHR2/HZJT5iiEtjAHO+g25dwxn7o1RUmgBNDilYMSacCoiFBsfEEd7sAUyngsCh+IEhkiERUNmjc3VfnUVfWLdsXE3ulWhjZMQClm0P6Q7amOvgGpzzVC+cS9RQAAAABJRU5ErkJggg=="/>
+                </svg>
+              </span>+48 734 164 767
+            </a>
+          </p>
+          <p class="footer__contact__details__email">
+            <a class="footer__contact__link" href="mailto:kontakt@fronaweb.pl">
+              <span role="img" aria-label="email" class="footer__icon icon icon--mail">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24" height="18" viewBox="0 0 24 18">
+                  <image id="email" width="24" height="18" xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAASCAYAAABB7B6eAAAAAXNSR0IArs4c6QAAADhlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAAqACAAQAAAABAAAAGKADAAQAAAABAAAAEgAAAACLTU82AAABm0lEQVQ4Eb2VPUjDUBSFX6KCGFRQFMFFdNNJdLUodhBER7eO0kmkIFLBXVxEdLF2EMTFQRB0EOuizi7tpIOgOEldLLUglPjdmpYYmrSp0Qun97z7c+5LXpJqpmkqozexppSaBC/ABL+1PgSewGqzpXSOj4MOax2Em8+/RvM6u9+FZEAnqgcBKKfQ0sAZ2staW8+e3JJ7MEqwQHAJvtPgoE005E7IbU/hwjLgHdIO3sAUBRmSYfgFaAL1WoTeI3q7abgGI+BDt3VLIk3BAoVX8EHwaMu70SyJcUs8BJfDFXGxon3Ad0ipY4Zs0PAMhgjKA+BmtyT6qbujJwaXnRv24moDJB+n4UQIzXO4LeEOS5ILgU9qk+Sq1Sj7GTj6S8s0v9OIZBFZhO9bRTFi28RaWV+CCSvudLlaA6ShAGYQvEFwFt4CP4WPwUW8C7hZXQPKzVGES1eAeITgYTnh4XPlN9mjppJKIDzAqgjWK9EaxM8AkZJvli9ze4p8iXgV/8uAvxyii/iPN8/rchvIGXLIK2AYBPFHY9+DxuLhC95nhdkhMLIMAAAAAElFTkSuQmCC"/>
+                </svg>
+              </span>kontakt@fronaweb.pl
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
 <?php wp_footer(); ?>
 </body>
 </html>
